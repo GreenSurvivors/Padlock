85b332a636cbd9444e8cefe90380d39a889f5f89
FireInstall <feuerreiter@greensurvivors.de>
2024-06-03 23:09:35 +0200

    make CraftAnvilInventory final.

--- src/main/java/de/greensurvivors/padlock/impl/internal/v20_6/CraftAnvilInventory.java
+++ src/main/java/de/greensurvivors/padlock/impl/internal/v20_6/CraftAnvilInventory.java
@@ -7,8 +7,8 @@ import org.bukkit.craftbukkit.inventory.CraftInventoryAnvil;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.Nullable;
 
-public class CraftAnvilInventory extends CraftInventoryAnvil implements InputAnvilMenu {
-    protected NMSInputAnvilMenu menu;
+public final class CraftAnvilInventory extends CraftInventoryAnvil implements InputAnvilMenu {
+    private final NMSInputAnvilMenu menu;
 
     public CraftAnvilInventory(Location location, Container inventory, Container resultInventory, NMSInputAnvilMenu container) {
         super(location, inventory, resultInventory, container);
